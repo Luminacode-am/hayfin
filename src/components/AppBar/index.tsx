d31b251fc3f56a208b452240ab3b6058ba6c2b92
LilyHov <lilyhovasapyan@gmail.com>
2025-10-30 18:42:14 +0400

    added appbar init version

---
+++ src/components/AppBar/index.tsx
@@ -0,0 +1,15 @@
+import React from  'react'
+
+const AppBar: React.FC = () => {
+return (
+    <header className='border-b shadow flex'>   
+    <img 
+        className="w-18 h-16 shadow-xl rounded-md"
+        alt="logo" 
+        src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'/>
+        Sign in/ Signup
+    </header>
+)
+}
+
+export default AppBar
---
+++ src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AppBar from '../components/AppBar';
+
+function App() {
+  return (
+    <div className="">
+      <AppBar/>
+  </div>
+  );
+}
+
+export default App;
+
