f1d3e3c189b7975ceb01ce5d84620e4f8bed6cc0
Davit-77 <dhakobyan750@gmail.com>
2025-10-30 21:47:54 +0400

    header and footer
    Footer failed

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -2,12 +2,12 @@ import React from  'react'
 
 const AppBar: React.FC = () => {
 return (
-    <header className='border-b shadow flex'>   
+    <header className='border-b shadow flex  max-w-6xl m-auto items-center justify-between'>   
     <img 
         className="w-18 h-16 shadow-xl rounded-md"
         alt="logo" 
-        src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'/>
-        Sign in/ Signup
+        src='https://www.vectorlogo.zone/logos/apple/apple-tile.svg'/>
+        <a href="#" className='flex items-center text-teal-400' >Sign in/ Sign Up</a>
     </header>
 )
 }
---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+    return (
+        <footer className='border-b shadow flex  max-w-6xl m-auto items-center justify-between'>
+           <p>Let’s work together</p>
+        </footer>
+
+    )
+}
+
+export default Footer
