660e97b9d8e415e61ffccf2f87d654e9b9285487
Davit-77 <dhakobyan750@gmail.com>
2025-10-31 22:39:43 +0400

    footer and favico

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from  'react'
 
 const Footer: React.FC = () => {
     return (
         <footer className='border-b shadow flex  max-w-6xl m-auto items-center justify-between'>
            <p>Let’s work together</p>
+           <div className='flex'>
+           <img src="/src/Vector.png" alt="instagram"/>
+           <img src="/src/Social link 2.png" alt="Linkdld"/>
+           <img src="/src/Social link 3.png" alt="X"/>
+           </div>
         </footer>
 
     )
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import AppBar from '../components/AppBar';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <div className="">
       <AppBar/>
+      <Footer/>
   </div>
   );
 }
