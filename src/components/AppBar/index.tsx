0bee0f8336f9d4683e38f3577d1a16dc6949a1d3
SamvelLalayan2007 <skairnskahd@gmail.com>
2025-11-06 18:11:30 +0400

    init

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -5,7 +5,7 @@ return (
     <header className='border-b shadow flex'>   
     <img 
         className="w-18 h-16 shadow-xl rounded-md"
-        alt="logo" 
+        alt="lo" 
         src='https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png'/>
         Sign in/ Signup
     </header>
