545d07628bfcd90dd8ffaa313b2b0c3999bd0a36
SamvelLalayan2007 <skairnskahd@gmail.com>
2025-11-11 17:14:47 +0400

    commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'react-app',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier'
+    ],
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+      'prettier/prettier': ['error'],
+      'react/react-in-jsx-scope': 'off',
+    },
+  };
+  
---
+++ src/pages/App.tsx
@@ -0,0 +1,17 @@
+import Hero from "../components/Hero";
+import AppBar from "../components/AppBar";
+import Footer from "../components/Footer";
+
+function App() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <AppBar />
+      <main className="flex-grow">
+        <Hero />
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
