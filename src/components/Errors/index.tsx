bf83bf38790725e5351d33486151b95ccfb0e1a4
Garik-lang <garikbastegyan556@gmail.com>
2025-11-16 14:57:38 +0400

    .

--- src/components/Errors/index.tsx
+++ src/components/Errors/index.tsx
@@ -5,7 +5,7 @@ const Errors :React.FC = () => {
     <>
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="text-center">
-          <p className="text-base font-semibold text-indigo-600">404</p>
+          <p className="text-9xl font-semibold taxt-emerald-900">404</p>
           <h1 className="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
             Page not found
           </h1>
@@ -15,11 +15,11 @@ const Errors :React.FC = () => {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="#"
-              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-emerald-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-emerald-700 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-emerald-700"
             >
               Go back home
             </a>
-            <a href="#" className="text-sm font-semibold text-gray-900">
+            <a href="#" className="text-sm font-semibold text-gray-900 hover:text-emerald-700">
               Contact support <span aria-hidden="true">&rarr;</span>
             </a>
           </div>
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,13 +2,14 @@
 import Hero from "../components/Hero";
 import AppBar from "../components/AppBar";
 import Footer from "../components/Footer";
+import Errors from "../components/Errors";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <AppBar />
       <main className="flex-grow">
-        <Hero />
+        <Errors />
       </main>
       <Footer />
     </div>
