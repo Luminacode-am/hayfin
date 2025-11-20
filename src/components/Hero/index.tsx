1c2eaf5ca6f4fb0d9292c27d598aa4e4b0d4097e
Amailya <amalyamxitaryan2020@gmail.com>
2025-11-20 16:05:11 +0400

    card

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,38 +1,4 @@
 
-// import { ArrowRight } from "lucide-react";
-// import heroImage from './finance.png'
-
-// export default function Hero() {
-//   return (
-//     <section className="bg-gradient-to-b from-blue-50 to-white py-20 px-6">
-//       <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-10">
-     
-//         <div className="text-center md:text-left md:w-1/2">
-//           <h1 className="text-4xl sm:text-5xl font-bold text-gray-900 mb-4 leading-tight">
-//             Building trust in every transaction.
-//           </h1>
-//           <p className="text-lg text-gray-600 mb-8">
-//             Your reliable partner in finance and growth.
-//           </p>
-//           <button className="inline-flex items-center gap-2 bg-blue-600 text-white px-6 py-3 rounded-xl font-semibold hover:bg-blue-700 transition">
-//             Get Started <ArrowRight size={18} />
-//           </button>
-//         </div>
-//         <div className="md:w-1/2 flex justify-center">
-//           <img
-//             src={heroImage}
-//             alt="Finance illustration"
-//             className="w-full max-w-md h-auto"
-//           />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
-
-
-
-
 import Card from "./card";
      
 const cards = [
@@ -61,18 +27,20 @@ export default function Advantages() {
     The advantage and importance of this website in todays world
     </h1>
     </div>
-    <div className="md:w-1/3 flex flex-col gap-6 items-end ">
-          {cards.map((item) => (
-          <Card
-  key={item.title}
-  className="flex flex-col justify-between p-8  border border-neutral-400 bg-neutral-400 "
->
-  <h3 className="text-lg font-semibold text-black mb-4 mt-2 ml-2">{item.title}</h3>
-  <p className="text-base text-zinc-700 mt-2 ml-2">{item.description}</p>
-</Card>
-          ))}
-        </div>
+    <div className="w-full flex flex-col gap-6">
+  {cards.map((item) => (
+    <Card
+      key={item.title}
+      className="flex flex-col justify-between p-8 border border-neutral-400 bg-neutral-400"
+    >
+      <h3 className="text-lg font-semibold text-black mb-4 mt-2">
+        {item.title}
+      </h3>
+      <p className="text-base text-zinc-700 mt-2">{item.description}</p>
+    </Card>
+  ))}
+</div>
       </div>
     </section>
   );
-}
+};
