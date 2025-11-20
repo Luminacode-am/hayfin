e548518943ce128c72715d3b79fc4363bd66de16
Davit-77 <dhakobyan750@gmail.com>
2025-11-20 18:06:27 +0400

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,17 @@
-import Hero from '../components/Hero';
-import AppBar from '../components/AppBar';
-import Footer from '../components/Footer';
-
+import Hero from "../components/Hero";
+import AppBar from "../components/AppBar";
+import Footer from "../components/Footer";
+import CardList from "../components/CardList";
+import VCardList from "../components/VCardList";
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <AppBar />
-      <main className="grow">
+      <main className="flex-grow">
         <Hero />
+        <CardList />
+         <VCardList />
+
       </main>
       <Footer />
     </div>
