419ba9493e4a59babd949c12ddf5595198890d53
Davit-77 <dhakobyan750@gmail.com>
2025-11-20 17:34:25 +0400

    cards

---
+++ src/components/VCardList/index.tsx
@@ -0,0 +1,38 @@
+import Card from "../Card";
+
+const cards = [
+    {
+        name: "Gold Vip", status: "For individuals", price: "$2", sale: "FREE/month",
+        services:
+     `Feature 1
+    Feature 2 
+    Feature 3`},
+    {
+        name: "Gold Vip", status: "For individuals", price: "$2", sale: "FREE/month",
+        services:
+    `Feature 1
+    Feature 2 
+    Feature 3`},
+    {
+        name: "Gold Vip", status: "For individuals", price: "$2", sale: "FREE/month",
+        services:
+    `Feature 1
+    Feature 2 
+    Feature 3`
+    }
+]
+export default function Cards() {
+    return(
+          <div className="flex gap-[50px] justify-center pb-[48px]  ">
+      {cards.map((card) => (
+        <Card key={card.name} className="flex flex-col justify-between">
+        <div>
+            <h2>{card.name}</h2>
+        </div>
+          
+          
+        </Card>
+      ))}
+    </div>
+    )
+}
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,26 @@
+<<<<<<< Updated upstream
 import Hero from '../components/Hero';
 import AppBar from '../components/AppBar';
 import Footer from '../components/Footer';
 
+=======
+import Hero from "../components/Hero";
+import AppBar from "../components/AppBar";
+import Footer from "../components/Footer";
+import CardList from "../components/CardList";
+import VCardList from "../components/VCardList";
+>>>>>>> Stashed changes
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
       <AppBar />
       <main className="grow">
         <Hero />
+<<<<<<< Updated upstream
+=======
+         <VCardList />
+        <CardList />
+>>>>>>> Stashed changes
       </main>
       <Footer />
     </div>
