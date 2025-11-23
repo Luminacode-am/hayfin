23d048f768524964777e0a3e8050131274c26c18
Garik-lang <garikbastegyan556@gmail.com>
2025-11-23 12:01:34 +0400

    Inputs

--- src/components/Inputs/index.tsx
+++ src/components/Inputs/index.tsx
@@ -4,22 +4,36 @@ import { Input } from '@headlessui/react';
 
 type InputType = 'button' | 'email' | 'number' | 'text' | 'password';
 type InputBorder = 'xl' | '2xl' | '3xl';
-type InputColor = 'red' | 'blue' | 'green' | 'teal'
-type InputTextColor = 'white' | 'black' | 'grey'
+type InputColor = 'red' | 'blue' | 'green' | 'teal';
+type InputTextColor = 'white' | 'black' | 'gray';
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+
+interface InputPops extends React.InputHTMLAttributes<HTMLInputElement> {
     type?: InputType;
     border?: InputBorder;
-    color?: InputColor;
+    colors?: InputColor;
     textColor?: InputTextColor;
 }
 
-const InputStyle: React.FC<InputProps> = (
+const InputStyle: React.FC<InputPops> = ({
+  children,
   type = 'number',
   border = 'xl',
-  color = 'red', 
+  color = 'red',
+  textColor = 'gray', 
+  className, 
+  disabled,
+  ...props
   
-)
+}) => {
+const InputStyles: Record<InputType, Record<InputBorder, string>> = {
+number: {
+  gray: 'pl-10 bg-gray-400',
+  white: 'pl-10 bg-white'
+  black: 'pl-10 bg-black'
+},
+}
+}
 
 
-
+export default InputStyle;
