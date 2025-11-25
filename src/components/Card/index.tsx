47f42a308a672acbd779ba5f1effa10ba077c44d
LilyHov <lilyhovasapyan@gmail.com>
2025-11-25 19:03:29 +0400

    added config script for prettier

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -6,7 +6,12 @@ interface CardProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   key?: string;
 }
 
-const Card: React.FC<CardProps> = ({ children, border = "none", key, ...props }) => (
+const Card: React.FC<CardProps> = ({
+  children,
+  border = 'none',
+  key,
+  ...props
+}) => (
   <div
     key={key}
     className="w-[336px] bg-white max-h-[280px] rounded-[16px] border-[#E6E6E6] border flex flex-col justify-between"
