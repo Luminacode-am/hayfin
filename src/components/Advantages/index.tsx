8f46ca666c1f70620afebbda25a83c2b62ab6335
Amailya <amalyamxitaryan2020@gmail.com>
2025-11-29 15:04:05 +0400

    Advantages

--- src/components/Advantages/index.tsx
+++ src/components/Advantages/index.tsx
@@ -46,7 +46,7 @@ export default function Advantages() {
                   {card.description}
                 </p>
               </div>
-            </Card>
+           </Card>
           ))}
         </div>
       </div>
