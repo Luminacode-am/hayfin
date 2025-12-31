7796e4947fba3c583b99381ff84ec0b76528c1d6
Garik-lang <garikbastegyan556@gmail.com>
2025-12-31 14:01:09 +0400

    .

--- src/components/ProfileHeader/index.tsx
+++ src/components/ProfileHeader/index.tsx
@@ -1,23 +1,14 @@
 import { Disclosure } from '@headlessui/react';
 import { Menu } from 'lucide-react';
+import { useState } from "react";
 
 
 export default function PHeader(){
+    {/*Desktop vershion*/}
      function Burger(){
         const items = ["Settings", "Contacts", "Notifications"];
-    }
-    return(
-        <Disclosure as="nav" className="bg-white shadow-xs">
-        {({ open }) => ( 
-            <>  
-            {/*Desktop*/}
-         <header className="flex sm:justify-around">
-              <div className="flex items-center">
-                  <p className="text-xl">
-                   My <span className="text-cyan-600">account</span> 
-                 </p>
-              </div>
-              <div className="flex items-center">
+        return(
+                <div className="flex items-center">
                   <ol className="text-lg gap-32px">
                     {items?.map((item, index) => (
                      <li key={index} className="text-cyan-600 hover:text-cyan-700 transition">
@@ -28,11 +19,64 @@ export default function PHeader(){
                     ))}
                   </ol>
               </div>
+        )
+    Burger()
+    }
+    {/*Mobile vershion*/}
+    function Dropdown(){
+        const [open, setOpen] = useState(false);
+
+        const items = ["Settings", "Contacts", "Notifications"];
+
+        const App = () => {
+        return(
+            <div onClick={() => setOpen(!open)}>
+                <Menu />
+
+                {open && (
+                  <div className='flex flex-col bg-gray-50 max-w-200px max-h-300px'>
+                    <ol className="text-lg gap-32px">
+                      {items?.map((item, index) => (
+                          <li key={index} className="text-cyan-600 hover:text-cyan-700 transition">
+                              <a href="#">
+                                 {item}
+                              </a>
+                           </li>
+                       ))}
+                    </ol>
+                  </div>
+                )}
+            </div>
+        )
+    }
+    Dropdown()
+    }
+
+    return(
+        <Disclosure as="nav" className="bg-white shadow-xs">
+        {({ open }) => ( 
+            <>  
+            {/*My profile*/}
+         <header className="flex sm:justify-around">
+              <div className="flex items-center">
+                  <p className="text-xl">
+                   My <span className="text-cyan-600">account</span> 
+                 </p>
+              </div>
           </header>
           
-
+          {/*Mobile*/}
+            <header className="flex sm:justify-around">
+              <div className="flex items-center">
+                  <p className="text-lg">
+                   My <span className="text-cyan-600">account</span> 
+                 </p>
+              </div>
+          </header>
           </> 
           )}
         </Disclosure>
     );
 }
+
+PHeader()
